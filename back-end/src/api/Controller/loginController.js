5e219b9573ef94a1fb07fcf00d0d697e02ea48c0
vinicius leandro <leandrocostavinicius@gmail.com>
2023-01-24 20:37:13 -0300

    Fix find no banco de dados

--- back-end/src/api/Controller/loginController.js
+++ back-end/src/api/Controller/loginController.js
@@ -4,7 +4,7 @@ class loginController {
   static async login(req, res) {
     const login = req.body;
 
-    const result = await loginService.login(login);
+    const result = await loginService(login);
 
     return res.status(200).json({ token: result.token });
   }
