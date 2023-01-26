29669450795db94d765e8e06f5f932863b57aec3
Patricia Correia <pncorreia25@gmail.com>
2023-01-26 09:25:30 -0300

    req 22 e 23 erros de lint

--- back-end/src/api/Controller/orderController.js
+++ back-end/src/api/Controller/orderController.js
@@ -3,10 +3,9 @@ const productService = require('../Services/orderService');
   const getAll = async (_req, res) => {
     const orders = await productService.getAll();
 
-    return res.status(200).json(orders)
-  }
+    return res.status(200).json(orders);
+  };
 
 module.exports = {
   getAll,
 };
-
--- back-end/src/api/Services/orderService.js
+++ back-end/src/api/Services/orderService.js
@@ -3,7 +3,7 @@ const { Sale } = require('../../database/models');
   const getAll = async () => {
     const orderList = await Sale.findAll();
     return orderList;
-  }
+  };
 
 module.exports = {
   getAll,
