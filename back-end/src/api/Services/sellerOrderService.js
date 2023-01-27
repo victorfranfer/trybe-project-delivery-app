bc1e7c043519da602cf2edf3df7b345c44380d87
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-01-27 16:41:54 -0300

    mudança na requisição, ainda não passando

--- back-end/src/api/Services/sellerOrderService.js
+++ back-end/src/api/Services/sellerOrderService.js
@@ -5,12 +5,18 @@ const getAll = async () => {
   return salesList;
 };
 
-  const getAllBySellerId = async (sellerId) => {
-    const salesListBySellerId = await Sale.findAll({ where: sellerId });
-    return salesListBySellerId;
+const getAllBySellerId = async (sellerId) => {
+  const salesListBySellerId = await Sale.findAll({ where: sellerId });
+  return salesListBySellerId;
+  };
+
+const getOrderBySellerId = async (id, role) => {
+  const salesListBySellerId = Sale.findAll({ where: { id, role } });
+  return salesListBySellerId;
   };
 
   module.exports = {
     getAll,
     getAllBySellerId,
+    getOrderBySellerId,
   };
