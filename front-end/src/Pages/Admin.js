cfcb3f5f177657b2cbbd9a6c9aabf8a983114afc
vinicius leandro <leandrocostavinicius@gmail.com>
2023-02-01 18:24:06 -0300

    Refatorando codigo, em vez de props é usado context agora

--- front-end/src/Pages/Admin.js
+++ front-end/src/Pages/Admin.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../Components/Header';
 import FormCreateUser from '../Components/FormCreateUser';
 import NameFormCreateUser from '../Components/NameFormCreateUser';
 import UserTable from '../Components/UserTable';
-import { UserTableProvider } from '../Context/UserTableContex';
+import { AdminProvider } from '../Context/AdminContex';
 
 export default function Admin() {
-  const [createUserError, setCreateUserError] = useState(false);
   return (
-    <UserTableProvider>
+    <AdminProvider>
       <Header />
       <div>
-        <NameFormCreateUser createUserError={ createUserError } />
-        <FormCreateUser setCreateUserError={ setCreateUserError } />
+        <NameFormCreateUser />
+        <FormCreateUser />
       </div>
       <div>
         <h2>Lista de usuários</h2>
         <UserTable />
       </div>
-    </UserTableProvider>
+    </AdminProvider>
   );
 }
