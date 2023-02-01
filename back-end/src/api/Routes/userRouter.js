7d4c85998fa92b6b8b6ef1a2039a3436bd2d9030
vinicius leandro <leandrocostavinicius@gmail.com>
2023-02-01 17:01:16 -0300

    Adiciona rota, controller e service para pegar todos os usuarios

--- back-end/src/api/Routes/userRouter.js
+++ back-end/src/api/Routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getSellers, getUserByEmail } = require('../Controller/userController');
+const { getSellers, getUserByEmail, getAllUsers } = require('../Controller/userController');
+const validateTokenMiddleware = require('../Middlewares/validateToken');
 
 const router = express.Router();
 
+router.get('/', validateTokenMiddleware, getAllUsers);
 router.get('/sellers', getSellers);
 router.post('/email', getUserByEmail);
 
