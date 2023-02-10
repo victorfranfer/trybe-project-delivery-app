725727a22f892e4c51973e4ad5b778290a58af3d
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2023-02-10 19:13:43 -0300

    request

--- front-end/src/Services/Request.js
+++ front-end/src/Services/Request.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
-
-const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
-const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001';
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 
 const api = axios.create({
   baseURL: `${PROTOCOL}://${HOST}`,
   timeout: 10000,
   headers: {
     'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
 export const get = async (endpoint) => {
